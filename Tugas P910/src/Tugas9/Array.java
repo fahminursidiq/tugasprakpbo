ee492fa54dfc1d7bb03009e9fe095d8191a472ae
fahminursidiq <fahminursidiq59@gmail.com>
2021-06-02 20:18:22 +0700

    menambahkan tugas prak pbo 9 dan 10

---
+++ Tugas P910/src/Tugas9/Array.java
@@ -0,0 +1,15 @@
+package Tugas9;
+
+public class Array {
+		 public static void main(String[] args) {
+
+		        // membuat array
+		        int angka[] = {6,1,90,33,28};
+
+		        // menggunakan perulangan For each untuk menampilkan angka
+		        for( int x : angka ){
+		            System.out.print(x + " ");
+		        }
+
+		    }
+	}
---
+++ Tugas P910/src/Tugas9/For.java
@@ -0,0 +1,11 @@
+package Tugas9;
+
+public class For {
+	public static void main(String[] args){
+
+	     for(int i=0; i <= 7; i++){
+	     System.out.println("*****");
+	        }
+
+	    }
+	}
